729fd2c3148d8035c473a265925531814e868321
Mike <mitghi@me.com>
2021-06-25 09:50:52 +0200

    Convert string to hex

--- intercom_flutter/ios/Classes/IntercomFlutterPlugin.m
+++ intercom_flutter/ios/Classes/IntercomFlutterPlugin.m
@@ -122,8 +122,18 @@ id unread;
     } else if([@"sendTokenToIntercom" isEqualToString:call.method]){
         NSString *token = call.arguments[@"token"];
         if(token != (id)[NSNull null] && token != nil) {
-            NSData* encodedToken=[token dataUsingEncoding:NSASCIIStringEncoding];
-            [Intercom setDeviceToken:encodedToken];
+            NSMutableData *stringData = [[NSMutableData alloc] init];
+            unsigned char whole_byte;
+            char byte_chars[3] = {'\0','\0','\0'};
+            int i;
+            for (i=0; i < [token length] / 2; i++) {
+                byte_chars[0] = [token characterAtIndex:i*2];
+                byte_chars[1] = [token characterAtIndex:i*2+1];
+                whole_byte = strtol(byte_chars, NULL, 16);
+                [stringData appendBytes:&whole_byte length:1];
+            }
+
+            [Intercom setDeviceToken:stringData];
             result(@"Token set");
         }
     } else if([@"displayArticle" isEqualToString:call.method]) {
